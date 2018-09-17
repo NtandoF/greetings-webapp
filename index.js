3c0091910443da2d2aa5ac41fb333be63ca3ca8a
Ntando <ntandofitshaneversace@gmail.com>
2018-09-17 15:02:11 +0200

    made changes to my css

--- index.js
+++ index.js
@@ -114,7 +114,7 @@ app.get('/home', function (req, res) {
     res.redirect('/');
 })
 
-let PORT = process.env.PORT || 30781;
+let PORT = process.env.PORT || 3088;
 
 app.listen(PORT, function () {
     console.log('App running on port', PORT);
